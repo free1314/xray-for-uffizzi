b2fb439f8d09466eeb074431426f0b630ff063cc
free1314 <96565348+free1314@users.noreply.github.com>
2023-02-06 15:03:31 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # 定义 UUID 及伪装路径、哪吒面板参数，请自行修改. (注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID='de04add9-5c68-8bab-950c-08cd5320df18'
-VMESS_WSPATH='/vmess'
+UUID = 'c1690a2d-91f6-4337-9523-d182e4c04f4c'
+VMESS_WSPATH=' /vmess '
 VLESS_WSPATH='/vless'
 TROJAN_WSPATH='/trojan'
 SS_WSPATH='/shadowsocks'
